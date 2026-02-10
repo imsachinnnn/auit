a4a94ac62e8847411f2421b833b14e2aeae159a5
Sachin A <sachinytr@gmail.com>
2026-02-10 23:14:09 +0530

    app v1.1

---
+++ ssm/app/src/main/java/com/onrender/ssms_web/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package com.onrender.ssms_web.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
